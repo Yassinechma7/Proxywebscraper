0b66ee776799a556f6378b7ae5cdd39a6ce4e9ae
Yassinechma7 <83378340+Yassinechma7@users.noreply.github.com>
2021-04-29 00:21:16 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,10 +2,11 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+###################################################
 url = "https://free-proxy-list.net/"
 html_doc = requests.get(url).text
 soup = BeautifulSoup(html_doc, 'lxml')
-
+###################################################
 
 def getProxyList():
 
@@ -13,6 +14,6 @@ def getProxyList():
 		proxy_list = ':'.join([item.text for item in items.select("td")[:2]])
 		print(proxy_list)
 
-
+###################################################
 if __name__ == "__main__":
 	getProxyList()
